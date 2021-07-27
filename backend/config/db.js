cb980658fc6125b923cccc8e7cb56b7c11a13492
topbrightdev <topbrightdev@gmail.com>
2021-07-27 10:19:29 +0530

    backend added

---
+++ backend/config/db.js
@@ -0,0 +1,28 @@
+const config = require('config') ;
+const db = config.get('mongoURI');
+const mongoose =require('mongoose');
+
+
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(db,
+            { 
+                useUnifiedTopology: true ,
+                useNewUrlParser: true ,
+                useCreateIndex: true , 
+                useFindAndModify:false
+            });
+        console.log('MongoDB Connected .....')
+
+    }catch(err){
+
+        
+        console.error(err.message);
+        //Exit process with Failure
+        process.exit(1);
+
+    }
+}
+
+module.exports =connectDB;
